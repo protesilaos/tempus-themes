29de7547d74ccfeb43ddffa918515d8ab5242d0e
Protesilaos Stavrou <info@protesilaos.com>
2018-07-08 14:58:30 +0300

    Add Tempus Day (WCAG AA), Tempus Night (WCAG AAA).

---
+++ st/tempus_day.h
@@ -0,0 +1,29 @@
+static const char *colorname[] = {
+    "#3b2820",
+    "#d62500",
+    "#207f20",
+    "#8a6900",
+    "#306ad0",
+    "#c6365d",
+    "#00798d",
+    "#f5f0f5",
+    "#5b4440", 
+    "#bb4b00", 
+    "#5a7800",
+    "#9a6033",
+    "#6061d0",
+    "#8850d0",
+    "#007c6d",
+    "#f8f1e0",
+};
+
+/* Default colors (colorname index)
+ * foreground, background, cursor, reverse cursor
+ */
+unsigned int defaultbg = 15;
+unsigned int defaultfg = 0;
+static unsigned int defaultcs = 0;
+static unsigned int defaultrcs = 15;
+static unsigned int mousefg = 8;
+static unsigned int mousebg = 7;
+
---
+++ st/tempus_night.h
@@ -0,0 +1,29 @@
+static const char *colorname[] = {
+    "#1a1a1a",
+    "#fb7e8e",
+    "#4ab96d",
+    "#b0a800",
+    "#5aaaf2",
+    "#ee80c0",
+    "#55b1c3",
+    "#c4bdaf",
+    "#18143d", 
+    "#f69d6a", 
+    "#88c400",
+    "#d7ae00",
+    "#8cb4f0",
+    "#de99f0",
+    "#00ca9a",
+    "#e0e0e0",
+};
+
+/* Default colors (colorname index)
+ * foreground, background, cursor, reverse cursor
+ */
+unsigned int defaultbg = 0;
+unsigned int defaultfg = 15;
+static unsigned int defaultcs = 15;
+static unsigned int defaultrcs = 0;
+static unsigned int mousefg = 7;
+static unsigned int mousebg = 8;
+
