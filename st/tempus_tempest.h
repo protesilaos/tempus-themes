dbfd744e4b4ef0967a91a0cbbf1630f227778e98
Protesilaos Stavrou <info@protesilaos.com>
2018-08-07 10:41:58 +0300

    Add Tempus Tempest

---
+++ st/tempus_tempest.h
@@ -0,0 +1,29 @@
+static const char *colorname[] = {
+    "#282b2b",
+    "#c6c80a",
+    "#7ad67a",
+    "#bfc94a",
+    "#60d4cd",
+    "#c0c4aa",
+    "#8ad0b0",
+    "#b0c8ca",
+    "#303434", 
+    "#d1d933", 
+    "#99e299",
+    "#bbde4f",
+    "#74e4cd",
+    "#d2d4aa",
+    "#9bdfc4",
+    "#b6e0ca",
+};
+
+/* Default colors (colorname index)
+ * foreground, background, cursor, reverse cursor
+ */
+unsigned int defaultbg = 0;
+unsigned int defaultfg = 15;
+static unsigned int defaultcs = 15;
+static unsigned int defaultrcs = 0;
+static unsigned int mousefg = 7;
+static unsigned int mousebg = 8;
+
